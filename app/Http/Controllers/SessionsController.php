ed48254758d5506772ffb56eca0341c872cfdd40
Tony <373886379@qq.com>
2019-07-28 04:26:02 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -10,6 +10,7 @@ class SessionsController extends Controller
     public function create(){
         return view('sessions.create');
     }
+
     public function store(Request $request)
     {
         $credentials = $this->validate($request, [
@@ -25,7 +26,13 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
-        return;
     }
+
+    public function destroy(){
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
 
 }
