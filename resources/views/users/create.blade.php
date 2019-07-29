3385567f73edfaea1d2f27e37e8daaba1a73f0dd
Tony <373886379@qq.com>
2019-07-29 12:58:00 +0000

    重置密码

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -25,7 +25,7 @@
 
                     <div class="form-group">
                         <label for="password">密码：</label>
-                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                        <input type="password" name="password" class="form-control" value="{{ old('passwords') }}">
                     </div>
 
                     <div class="form-group">
