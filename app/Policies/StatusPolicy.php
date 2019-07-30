5998c8457c2bbffa9c567cbdd5dabf91a57d7d90
Tony <373886379@qq.com>
2019-07-30 09:00:07 +0000

    删除微博

---
+++ app/Policies/StatusPolicy.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+use App\Models\Status;
+
+class StatusPolicy
+{
+    use HandlesAuthorization;
+
+    public function destroy(User $user,Status $status){
+        return $status->user_id===$user->id;
+    }
+}
